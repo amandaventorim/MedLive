755a5eeb0c29abbad3dc2c212c92ce73b06baecd
amandaventorim <ventorim.amanda@gmail.com>
2025-06-16 10:33:52 -0300

    todos os models feitos

--- data/sql/medico_sql.py
+++ data/sql/medico_sql.py
@@ -3,6 +3,7 @@ CREATE TABLE IF NOT EXISTS medico (
     idMedico INTEGER PRIMARY KEY,
     crm TEXT NOT NULL,
     statusProfissional TEXT NOT NULL,
-    FOREIGN KEY (idUsuario) REFERENCES usuario(idUsuario)
+    FOREIGN KEY (idMedico) REFERENCES usuario(idUsuario) # é necessário o idUsuario? o medico tem dois ids? idmedico e idusuario ou só idmedico? idmedico é igual a idusuario?
 );
 """
+
