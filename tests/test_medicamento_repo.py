928cf534e5e1b196fbf95a37791561c6f4d495f7
amandaventorim <ventorim.amanda@gmail.com>
2025-06-25 16:43:35 -0300

    ooooo

--- tests/test_medicamento_repo.py
+++ tests/test_medicamento_repo.py
@@ -1,6 +1,7 @@
 import sys
 import os
 from data.repo.medicamento_repo import *
+from data.model.medicamento_model import Medicamento
 
 class TestMedicamentoRepo:
     def test_criar_tabela_medicamento(self, test_db):
@@ -11,3 +12,14 @@ class TestMedicamentoRepo:
         assert resultado == True, "A criação da tabela 'medicamento' falhou."
 
     
+    def test_inserir_medicamento(self, test_db):
+        # Arrange
+        criar_tabela_medicamento()
+        medicamento_teste = Medicamento(0, "Medicamento Teste")
+        # Act
+        id_medicamento_inserido = inserir_medicamento(medicamento_teste)
+        # Assert
+        medicamento_db = obter_medicamento_por_id(id_medicamento_inserido)
+        assert medicamento_db is not None, "O medicamento inserido não foi encontrado no banco de dados."
+        assert medicamento_db.idMedicamento == 1, "O ID do medicamento inserido deve ser igual a 1"
+        assert medicamento_db.nome == "Medicamento Teste", "O nome do medicamento inserido deve ser igual ao nome inserido"
