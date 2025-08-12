b41cd2417725e9c4b143ff01c5d4f6eba231b565
amanda <ventorim.amanda@gmail.com>
2025-08-12 16:56:54 -0300

    mais coisa

--- routes/admin/admin_rotas.py
+++ routes/admin/admin_rotas.py
@@ -4,13 +4,13 @@ from fastapi.templating import Jinja2Templates
 router = APIRouter()
 templates = Jinja2Templates(directory="templates")
 
-# tem que fazer essa pagina ainda
 @router.get("/dashboard_admin")
-async def get_dashboard_medico():
+async def get_dashboard_admin():
     response = templates.TemplateResponse("/admin/dashboard_admin.html", {"request": {}})
     return response
 
 @router.get("/cadastro_admin")
-async def get_cadastro_medico():
+async def get_cadastro_admin():
     response = templates.TemplateResponse("/admin/cadastro_admin.html", {"request": {}})
     return response
+    
